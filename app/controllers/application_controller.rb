6eeff51b198ff8dabe9d6cfbd4b6abf48fb1dfe2
lamtu1 <tu.lam@oregonstate.edu>
2022-08-29 13:15:50 -0700

    Add new field into DB

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,14 @@ class ApplicationController < ActionController::Base
   include Blacklight::Controller
   layout :determine_layout if respond_to? :layout
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update) do |user_params|
+      user_params.permit(:nickname, :email, :password, :password_confirmation, :current_password)
+    end
+  end
+
 end
