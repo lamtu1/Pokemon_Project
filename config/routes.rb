eb1f8d8a71fd0644bbdc7f0aca13a302ea59b0b9
lamtu1 <tu.lam@oregonstate.edu>
2022-08-30 11:41:05 -0700

    Remove lines

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,4 @@ Rails.application.routes.draw do
       delete 'clear'
     end
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Defines the root path route ("/")
 end
