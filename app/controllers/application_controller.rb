98b8b5516a625981098354fc0edbd76bd8fa2349
lamtu1 <tu.lam@oregonstate.edu>
2022-09-06 11:40:48 -0700

    Add routes & stylize pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,17 @@ class ApplicationController < ActionController::Base
   # Add in params to allow things into Devise w/ Blacklight
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update) do |user_params|
-      user_params.permit(:nickname, :fav_pokemon, :quote, :email, :password, :password_confirmation, :current_password)
+      user_params.permit(:email, :nickname, :fav_pokemon, :quote, :password, :password_confirmation, :current_password)
     end
   end
 
+  # Redirect after login
+  def after_sign_in_path_for(resource)
+    stored_location_for(pokemons) || pokemon_main_path
+  end
+
+  # Redirect after update
+  def after_update_path_for(resource)
+    pokemon_main_path
+  end
 end
