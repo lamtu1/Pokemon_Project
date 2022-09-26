b89f52a97070329ba1ceafdb7c85a6000479284d
lamtu1 <tu.lam@oregonstate.edu>
2022-09-26 11:04:09 -0700

    Add in describe box

--- spec/models/element_spec.rb
+++ spec/models/element_spec.rb
@@ -2,5 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Element, type: :model do
   # ASSOCIATION TEST: Whether the association between Type & Pokemon exist
-  it { should have_and_belong_to_many(:pokemons) }
+  describe 'associations' do
+    it { should have_and_belong_to_many(:pokemons) }
+  end
 end
--- spec/models/pokemon_spec.rb
+++ spec/models/pokemon_spec.rb
@@ -2,5 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Pokemon, type: :model do
   # ASSOCIATION TEST: Whether the association between Pokemon & Type exist
-  it { should have_and_belong_to_many(:elements) }
+  describe 'associations' do
+    it { should have_and_belong_to_many(:elements) }
+  end
+
+  # IMAGE TEST: Testing the image attachment to see it would attach one image
+  describe 'image attached' do
+    it { should have_one_attached(:image) }
+  end
+
+  describe 'GET #index' do
+    before { gets :index }
+
+    it { should render_template('index') }
+  end
 end
